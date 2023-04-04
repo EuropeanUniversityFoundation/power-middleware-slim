89fc893930a9989ba979eb9e74ff8b0dbc921b5c
Tiago Simões <60974710+tiagosimoes-euf@users.noreply.github.com>
2023-04-04 17:50:44 +0200

    Removes 'power-middleware' from routes path

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,6 @@ use Slim\App;
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class)->setName('home');    
 
-    $app->get('/power-middleware/rest/{subpath}[/{id}]', \App\Action\PowerMiddleware\PowerMiddlewareGetAction::class)
+    $app->get('/rest/{subpath}[/{id}]', \App\Action\PowerMiddleware\PowerMiddlewareGetAction::class)
         ->setName('power-middleware');
 };
