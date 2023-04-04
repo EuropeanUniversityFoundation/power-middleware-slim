39e6614fd979243897a7a50106fb338a3ce0c060
Tiago Simões <tiago.simoes@uni-foundation.eu>
2023-04-04 18:24:16 +0200

    Adds twig to render new front page template at root

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,13 @@
 <?php
 
 use Slim\App;
-//use GuzzleHttp\Client;
+use Slim\Views\Twig;
 
 return function (App $app) {
-    $app->get('/', \App\Action\HomeAction::class)->setName('home');    
+    $app->get('/', function ($request, $response, $args) {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'index.html');
+    })->setName('root');
 
     $app->get('/rest/{subpath}[/{id}]', \App\Action\PowerMiddleware\PowerMiddlewareGetAction::class)
         ->setName('power-middleware');
